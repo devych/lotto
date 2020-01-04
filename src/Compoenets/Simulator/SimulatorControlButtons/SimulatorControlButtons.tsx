d3da1221bf9baf94323bc08a107abd4fd0cf4e30
ych Dev <devych.code@gmail.com>
2020-01-04 21:26:05 +0900

    맥 포맷을 위한 커밋

--- src/Compoenets/Simulator/SimulatorControlButtons/SimulatorControlButtons.tsx
+++ src/Compoenets/Simulator/SimulatorControlButtons/SimulatorControlButtons.tsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Dropdown } from "react-bootstrap";
 
 const SimulatorControlButtons = () => {
   return (
     <Container>
       <Row className="justify-content-md-center">
         <Col xs lg="auto">
-          <Button variant="success">시뮬레이션 시작</Button>
+          <Dropdown style={{ margin: "2px" }}>
+            <Dropdown.Toggle variant="info" id="dropdown-basic" size="sm">
+              시뮬레이션 횟수: 기본 100
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">100</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">1000</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">10000</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">100000</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">1000000</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Col>
         <Col xs lg="auto">
-          <Button variant="danger">시뮬레이션 정지</Button>
+          <Button variant="success"> 시작</Button>
+        </Col>
+        <Col xs lg="auto">
+          <Button variant="danger"> 정지</Button>
         </Col>
         <Col xs lg="auto">
           <Button variant="secondary">초기화</Button>
