b568fa7b448f9702c273eeae3036a4a7786ec61d
devych <devych.code@gmail.com>
2020-04-25 14:34:11 +0900

    BallChance 번호별 확률 console warning 해결 Card.text <p>태그 가 Table <div> 태그를 포함하지 못하여 나는 warning

--- src/Compoenets/BallChance/BallChance.tsx
+++ src/Compoenets/BallChance/BallChance.tsx
@@ -22,42 +22,40 @@ const BallChance = () => {
             <Card className="text-center" border={"secondary"}>
                 <Card.Header>보너스 번호를 제외한 번호별 출현 확률</Card.Header>
                 <Card.Body>
-                    <Card.Text>
-                        <Table striped bordered hover responsive="xl">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>번호</th>
-                                    <th>출현 횟수</th>
-                                    <th>출현 확률</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {ballChance.map((item, index) => {
-                                    return (
-                                        <tr>
-                                            <td
-                                                style={{
-                                                    color: "grey",
-                                                }}
-                                            >
-                                                {index + 1}
-                                            </td>
-                                            <td
-                                                style={{
-                                                    fontWeight: "bolder",
-                                                }}
-                                            >
-                                                {item[0]}
-                                            </td>
-                                            <td>{item[1]}</td>
-                                            <td>{item[2]} %</td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </Table>
-                    </Card.Text>
+                    <Table striped bordered hover responsive="xl">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>번호</th>
+                                <th>출현 횟수</th>
+                                <th>출현 확률</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {ballChance.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td
+                                            style={{
+                                                color: "grey",
+                                            }}
+                                        >
+                                            {index + 1}
+                                        </td>
+                                        <td
+                                            style={{
+                                                fontWeight: "bolder",
+                                            }}
+                                        >
+                                            {item[0]}
+                                        </td>
+                                        <td>{item[1]}</td>
+                                        <td>{item[2]} %</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </Table>
                 </Card.Body>
             </Card>
         </>
