4fb1bfe3be245900ddab7e520a6cc1c9e37e5ae4
devych <devych.code@gmail.com>
2020-04-29 23:17:40 +0900

    공통 인터페이스 추출 후 모듈화

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,16 @@
+export interface ILotto {
+    bnusNo: number;
+    drwNo: number;
+    drwNoDate: string;
+    drwtNo1: number;
+    drwtNo2: number;
+    drwtNo3: number;
+    drwtNo4: number;
+    drwtNo5: number;
+    drwtNo6: number;
+    firstAccumamnt: string;
+    firstPrzwnerCo: number;
+    firstWinamnt: number;
+    id: number;
+    totSellamnt: number;
+}
