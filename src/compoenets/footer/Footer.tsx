f9d6cf4bd699dcb2d9cf8e634c42d7b19f83862b
devych <devych.code@gmail.com>
2020-05-15 23:12:54 +0900

    내용없는 footer 삭제

--- src/compoenets/footer/Footer.tsx
+++ src/compoenets/footer/Footer.tsx
@@ -3,12 +3,14 @@ import React from "react";
 const Footer = () => {
     return (
         <>
-            <blockquote className="blockquote text-center">
+            {/* <blockquote className="blockquote text-center">
                 <p className="mb-0">get Lotto Information for fun</p>
                 <footer className="blockquote-footer">
                     developed by devych
                 </footer>
-            </blockquote>
+            </blockquote> */}
+            <br />
+            <br />
         </>
     );
 };
