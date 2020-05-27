3164bda4ab07579ce78ae57cf9e8c5072cf4c5cc
devych <devych.code@gmail.com>
2020-05-27 23:13:42 +0900

    footer 강제개행 추가

--- src/compoenets/footer/Footer.tsx
+++ src/compoenets/footer/Footer.tsx
@@ -3,6 +3,7 @@ import React from "react";
 const Footer = () => {
     return (
         <>
+            <br />
             <blockquote className="blockquote text-center">
                 <p className="mb-0">
                     원하는 기능이 있으면 아래 메일로 기능 요청을 해주세요
