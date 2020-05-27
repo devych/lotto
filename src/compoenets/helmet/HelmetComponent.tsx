f35ea8f89eda9c56f0301acd71bce8955d74cfa1
devych <devych.code@gmail.com>
2020-05-27 23:09:13 +0900

    helmet ogp img+ 실제 img 추가

--- src/compoenets/helmet/HelmetComponent.tsx
+++ src/compoenets/helmet/HelmetComponent.tsx
@@ -16,6 +16,7 @@ const HelmetComponent = ({ title, description, ogUrl }: IHelmet) => (
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:url" content={ogUrl} />
+        <meta property="og:image" content={"http://ttolotto.me/og-img.png"} />
     </Helmet>
 );
 
