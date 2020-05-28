b16f91b63a41c433ef33158fd21f405c3f74ffb2
devych <devych.code@gmail.com>
2020-05-28 17:09:20 +0900

    ogp 수정 및 기본 페이지 적용

--- src/compoenets/helmet/HelmetComponent.tsx
+++ src/compoenets/helmet/HelmetComponent.tsx
@@ -8,15 +8,20 @@ interface IHelmet {
 }
 
 const HelmetComponent = ({ title, description, ogUrl }: IHelmet) => (
-    <Helmet>
+    <Helmet
+        htmlAttributes={{
+            lang: "ko",
+            prefix: "og: http://ogp.me/ns#",
+        }}
+    >
         <title>{title}</title>
         <meta name="description" content={description} />
 
-        <meta property="og:type" content="website" />
+        <meta property="og:url" content={ogUrl} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:url" content={ogUrl} />
         <meta property="og:image" content={"http://ttolotto.me/og-img.png"} />
+        <meta property="og:type" content="website" />
     </Helmet>
 );
 
